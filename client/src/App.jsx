fa93d32a3e38597ff5ee003c5b2bfa0c795d0139
dipanshukrgit <dipanshuhub0411@gmail.com>
2025-10-24 17:12:34 +0530

    footer and homepage update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Banner from './components/Banner'
+import Footer from './components/Footer'
+import Freebook from './components/Freebook'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
       <Banner/>
+      <Freebook/>
+      <Footer/>
     </div>
   )
 }
